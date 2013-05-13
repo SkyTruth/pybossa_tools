034a7f8723e8ca63734e81b0b63d9aaa9bc2e6d6
Egil Moeller <egil.moller@freecode.no>
2013-05-13 17:33:44 +0200

    Support for multiple apps

--- createTasks.py
+++ createTasks.py
@@ -27,157 +27,157 @@ import django.conf
 django.conf.settings.configure(TEMPLATE_DIRS=(os.path.dirname(__file__),))
 
 
+class CreateTasks(object):
+    def handle_arguments(self):
+       # Arguments for the application
+       usage = "usage: %prog [options]"
+       parser = OptionParser(usage)
+       # URL where PyBossa listens
+       parser.add_option("-s", "--server", dest="api_url",
+                         help="PyBossa URL http://domain.com/", metavar="URL",
+                         default="http://localhost:5000/")
+       # API-KEY
+       parser.add_option("-k", "--api-key", dest="api_key",
+                         help="PyBossa User API-KEY to interact with PyBossa",
+                         metavar="API-KEY")
+       # Create App
+       parser.add_option("-c", "--create-app", action="store_true",
+                         dest="create_app",
+                         help="Create the application",
+                         metavar="CREATE-APP")
+       # Update template for tasks and long_description for app
+       parser.add_option("-u", "--update-template", action="store_true",
+                         dest="update_template",
+                         help="Update Tasks template",
+                         metavar="UPDATE-TEMPLATE")
 
-def handle_arguments():
-    # Arguments for the application
-    usage = "usage: %prog [options]"
-    parser = OptionParser(usage)
-    # URL where PyBossa listens
-    parser.add_option("-s", "--server", dest="api_url",
-                      help="PyBossa URL http://domain.com/", metavar="URL",
-                      default="http://localhost:5000/")
-    # API-KEY
-    parser.add_option("-k", "--api-key", dest="api_key",
-                      help="PyBossa User API-KEY to interact with PyBossa",
-                      metavar="API-KEY")
-    # Create App
-    parser.add_option("-c", "--create-app", action="store_true",
-                      dest="create_app",
-                      help="Create the application",
-                      metavar="CREATE-APP")
-    # Update template for tasks and long_description for app
-    parser.add_option("-u", "--update-template", action="store_true",
-                      dest="update_template",
-                      help="Update Tasks template",
-                      metavar="UPDATE-TEMPLATE")
+       # Update tasks question
+       parser.add_option("-q", "--update-tasks",
+                         type="int",
+                         dest="update_tasks",
+                         help="Update Tasks n_answers",
+                         metavar="UPDATE-TASKS")
 
-    # Update tasks question
-    parser.add_option("-q", "--update-tasks",
-                      type="int",
-                      dest="update_tasks",
-                      help="Update Tasks n_answers",
-                      metavar="UPDATE-TASKS")
+       parser.add_option("-t", "--create-task",
+                         dest="create_task",
+                         help="Create a task",
+                         metavar="CREATE-MORE-TASK")
 
-    parser.add_option("-t", "--create-task",
-                      dest="create_task",
-                      help="Create a task",
-                      metavar="CREATE-MORE-TASK")
+       # Modify the number of TaskRuns per Task
+       # (default 30)
+       parser.add_option("-n", "--number-answers",
+                         type="int",
+                         dest="n_answers",
+                         help="Number of answers per task",
+                         metavar="N-ANSWERS",
+                         default=30)
 
-    # Modify the number of TaskRuns per Task
-    # (default 30)
-    parser.add_option("-n", "--number-answers",
-                      type="int",
-                      dest="n_answers",
-                      help="Number of answers per task",
-                      metavar="N-ANSWERS",
-                      default=30)
+       parser.add_option("-a", "--application",
+                         dest="app_root",
+                         help="Application (directory) name",
+                         metavar="APP-DIR",
+                         default="drillpad")
 
-    parser.add_option("-a", "--application-config",
-                      dest="app_config",
-                      help="Application config file",
-                      metavar="APP-CONFIG",
-                      default="app.json")
+       parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
+       (options, args) = parser.parse_args()
 
-    parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
-    (options, args) = parser.parse_args()
+       if not options.create_app and not options.update_template\
+               and not options.create_task and not options.update_tasks:
+           parser.error("Please check --help or -h for the available options")
 
-    if not options.create_app and not options.update_template\
-            and not options.create_task and not options.update_tasks:
-        parser.error("Please check --help or -h for the available options")
+       if not options.api_key:
+           parser.error("You must supply an API-KEY to create an \
+                         application and tasks in PyBossa")
 
-    if not options.api_key:
-        parser.error("You must supply an API-KEY to create an \
-                      application and tasks in PyBossa")
+       self.options = options
 
-    return options
 
-def get_configuration():
-    options = handle_arguments()
+    def get_configuration(self):
+       self.handle_arguments()
 
-    # Load app details
-    try:
-        with file(options.app_config) as app_json:
-            app_config = json.load(app_json)
-    except IOError as e:
-        print "application config file is missing! Please create a new one"
-        exit(1)
+       # Load app details
+       try:
+           with file(os.path.join(self.options.app_root, 'app.json')) as app_json:
+               self.app_config = json.load(app_json)
+       except IOError as e:
+           print "application config file is missing! Please create a new one"
+           exit(1)
 
-    return (app_config, options)
+    def contents(self, filename):
+       return django.template.loader.render_to_string(os.path.join(self.options.app_root, filename), {})
 
-def contents(filename):
-    return django.template.loader.render_to_string(filename, {})
+    def find_app_by_short_name(self):
+        self.app = pbclient.find_app(short_name=self.app_config['short_name'])[0]
 
-def run(app_config, options):
-    def find_app_by_short_name():
-        return pbclient.find_app(short_name=app_config['short_name'])[0]
+    def setup_app(self):
+        self.find_app_by_short_name()
+        self.app.long_description = self.contents('long_description.html')
+        self.app.info['task_presenter'] = self.contents('template.html')
+        self.app.info['thumbnail'] = self.app_config['thumbnail']
+        self.app.info['tutorial'] = self.contents('tutorial.html')
 
-    def setup_app():
-        app = find_app_by_short_name()
-        app.long_description = contents('long_description.html')
-        app.info['task_presenter'] = contents('template.html')
-        app.info['thumbnail'] = app_config['thumbnail']
-        app.info['tutorial'] = contents('tutorial.html')
+        pbclient.update_app(self.app)
 
-        pbclient.update_app(app)
-        return app
-
-    def create_task(app):
+    def create_task(self):
         # Data for the tasks
-        task_info = json.loads(options.create_task)
-        task_info["question"] = app_config['question']
-        task_info["n_answers"] = options.n_answers
-        pbclient.create_task(app.id, task_info)
+        task_info = json.loads(self.options.create_task)
+        task_info["question"] = self.app_config['question']
+        task_info["n_answers"] = self.options.n_answers
+        pbclient.create_task(self.app.id, task_info)
 
-    pbclient.set('api_key', options.api_key)
-    pbclient.set('endpoint', options.api_url)
+    def __init__(self):
+        self.get_configuration()
 
-    if options.verbose:
-        print('Running against PyBosssa instance at: %s' % options.api_url)
-        print('Using API-KEY: %s' % options.api_key)
+        pbclient.set('api_key', self.options.api_key)
+        pbclient.set('endpoint', self.options.api_url)
 
-    if options.create_app:
-        pbclient.create_app(app_config['name'],
-                            app_config['short_name'],
-                            app_config['description'])
+        if self.options.verbose:
+            print('Running against PyBosssa instance at: %s' % self.options.api_url)
+            print('Using API-KEY: %s' % self.options.api_key)
 
-        app = setup_app()
-    else:
-        app = find_app_by_short_name()
+        if self.options.create_app:
+            pbclient.create_app(app_config['name'],
+                                app_config['short_name'],
+                                app_config['description'])
 
-    if options.create_task:
-        create_task(app)
+            self.setup_app()
+        else:
+            self.find_app_by_short_name()
 
-    if options.update_template:
-        print "Updating app template"
-        # discard return value
-        setup_app()
+        if self.options.create_task:
+            self.create_task(app)
 
-    if options.update_tasks:
-        def tasks(app): 
-            offset = 0
-            limit = 100
-            while True:
-                tasks = pbclient.get_tasks(app.id, offset=offset, limit=limit)
-                if len(tasks) == 0:
-                    break
-                for task in tasks:
-                    yield task
-                offset += len(tasks)
+        if self.options.update_template:
+            print "Updating app template"
+            # discard return value
+            self.setup_app()
 
-        def update_task(task, count):
-            print "Updating task: %s" % task.id
-            if 'n_answers' in task.info:
-                del(task.info['n_answers'])
-            task.n_answers = options.update_tasks
-            pbclient.update_task(task)
-            count[0] += 1
+        if self.options.update_tasks:
+            def tasks():
+                offset = 0
+                limit = 100
+                while True:
+                    tasks = pbclient.get_tasks(self.app.id, offset=offset, limit=limit)
+                    if len(tasks) == 0:
+                        break
+                    for task in tasks:
+                        yield task
+                    offset += len(tasks)
 
-        print "Updating task n_answers"
-        app = find_app_by_short_name()
+            def update_task(task, count):
+                print "Updating task: %s" % task.id
+                if 'n_answers' in task.info:
+                    del(task.info['n_answers'])
+                task.n_answers = self.options.update_tasks
+                pbclient.update_task(task)
+                count[0] += 1
 
-        n_tasks = [0]
-        [update_task(t, n_tasks) for t in tasks(app)]
-        print "%s Tasks have been updated!" % n_tasks[0]
+            print "Updating task n_answers"
+            find_app_by_short_name()
+
+            n_tasks = [0]
+            [update_task(t, n_tasks) for t in tasks()]
+            print "%s Tasks have been updated!" % n_tasks[0]
 
 if __name__ == "__main__":
-    run(*get_configuration())
+    CreateTasks()
