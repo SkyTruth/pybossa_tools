2c8d66434d313f9e19df27a7c67605e22c0e1700
Egil Moeller <egil.moller@freecode.no>
2013-05-28 12:37:54 +0200

    New app for images

---
+++ drillpadcatimg/maphandler.js
@@ -0,0 +1,19 @@
+var map;
+
+function loadMap() {
+}
+
+function setProgress(data) {
+  var pct = Math.round((data.done*100)/data.total);
+  $("#progress").css("width", pct.toString() +"%");
+  $("#progress").attr("title", pct.toString() + "% completed!");
+  $("#progress").tooltip({'placement': 'left'}); 
+  $("#total").text(data.total);
+  $("#done").text(data.done);
+}
+
+function updateMap(info) {
+  var img = $("<img>");
+  img.attr("src", info.url);
+  $("#map").html(img);
+}
