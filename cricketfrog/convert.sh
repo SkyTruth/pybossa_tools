ca003efb754ecd13336f30038ce5ec391a029dfe
Egil Moeller <egil.moller@freecode.no>
2013-09-12 15:25:01 +0200

    Better tutorial

--- cricketfrog/convert.sh
+++ cricketfrog/convert.sh
@@ -5,7 +5,7 @@ find "$1" -name "*.txt" |
       tr "	" "," |
       sed -e "s+ \([0-9]*\)$+,\1+g" |
       while IFS="," read longitude latitude year county url siteID bbox width height rest; do
-        bbox="$(echo $bbox | tr " " ",")"
+        bbox="$(echo $bbox | sed -e "s+\([-.0-9]*\) \([-.0-9]*\) \([-.0-9]*\) \([-.0-9]*\)$+\3,\4,\1,\2+g")"
         echo "{\"url\": \"$url\", \"bbox\": \"$bbox\", \"width\": $width, \"height\": $height, \"siteID\":\"$siteID\"}"
       done > "$(echo "$file" | sed -e "s+.txt$+.json+g")";
   done
