ca2e247a553d2c262f76ee3495da3562d2f67827
Egil Moeller <egil.moller@freecode.no>
2013-11-11 14:28:45 +0100

    Moved the code to handle IMG imagery to the base-openlayers app

--- base-openlayers/maphandler.js
+++ base-openlayers/maphandler.js
@@ -4,6 +4,10 @@ BaseOpenlayersApp = App = function() {
   app.mapIsLoaded = false;
 }
 
+App.prototype.useMapUnderlayes = false;
+App.prototype.taskProjection = new OpenLayers.Projection("EPSG:4326");
+App.prototype.taskZoom = 1.2;
+
 App.prototype.init = function () {
   var app = this;
   $(".expander-control").click(function (ev) {
@@ -35,19 +39,36 @@ App.prototype.loadMap = function() {
   app.mapIsLoaded = true;
 }
 
+App.prototype.loadMapCreateMapOptions = function () {
+  var app = this;
+  var options = {
+    div: "map",
+    controls: []
+  };
+  if (!app.useMapUnderlayes) {
+    options.allOverlays = true;
+    options.fractionalZoom =  true;
+    options.minScale = 442943842;
+    options.maxScale = 135;
+  }
+  return options;
+}
+
 App.prototype.loadMapCreateMap = function() {
   var app = this;
-  app.map = new OpenLayers.Map({
-      div: "map",
-      allOverlays: true,
-      fractionalZoom: true,
-      controls: [],
-      minScale: 442943842,
-      maxScale: 135
-  });
+  app.map = new OpenLayers.Map(app.loadMapCreateMapOptions());
 }
 App.prototype.loadMapAddLayers = function() {
   var app = this;
+
+  if (app.useMapUnderlayes) {
+    app.layers.osm = new OpenLayers.Layer.OSM();
+    app.map.addLayer(app.layers.osm);
+
+    app.layers.gmap = new OpenLayers.Layer.Google("Satellite", {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22});
+    app.map.addLayer(app.layers.gmap);
+  }
+
   app.layers.guide = new OpenLayers.Layer.Vector("Guide");
   app.layers.guide.id = "guide";
   app.map.addLayer(app.layers.guide);
@@ -57,7 +78,8 @@ App.prototype.loadMapAddControls = function() {
   app.map.addControls([
     new OpenLayers.Control.Navigation(),
     new OpenLayers.Control.Attribution(),
-    new OpenLayers.Control.PanZoomBar()
+    new OpenLayers.Control.PanZoomBar(),
+    new OpenLayers.Control.LayerSwitcher()
   ]);
 }
 
@@ -72,14 +94,24 @@ App.prototype.setProgress = function(data) {
 
 App.prototype.clearData = function() {
   var app = this;
-  app.map.zoomToExtent(app.getTaskBounds());
+  app.map.zoomToExtent(
+    app.getTaskBounds().transform(
+      App.prototype.taskProjection, app.map.getProjectionObject()
+    ).scale(app.taskZoom));
 }
 
 App.prototype.getTaskBounds = function() {
-  return this.taskBounds;
+  var bounds = new OpenLayers.Bounds();
+  [[app.info.longitude - app.info.size * oneMeter, app.info.latitude],
+   [app.info.longitude + app.info.size * oneMeter, app.info.latitude],
+   [app.info.longitude, app.info.latitude - app.info.size * oneMeter],
+   [app.info.longitude, app.info.latitude + app.info.size * oneMeter]].map(function (lonlat) {
+    bounds.extend(new OpenLayers.LonLat(lonlat[0], lonlat[1]));
+  });
+  return bounds;
 }
 
-App.prototype.loadImagery = function() {
+App.prototype.loadImageryWMS = function() {
   var app = this;
   if (app.map.getLayer('imagery')) app.map.removeLayer(app.map.getLayer('imagery'));
   var imagery = new OpenLayers.Layer.WMS(
@@ -91,50 +123,89 @@ App.prototype.loadImagery = function() {
   app.map.setLayerIndex(imagery, 0);
   app.map.setBaseLayer(imagery);
 }
+App.prototype.loadImageryIMG = function() {
+  // Note: This function only works if
+  // app.map.getProjectionObject() == app.taskProjection
+  var app = this;
+  if (app.map.getLayer('imagery')) app.map.removeLayer(app.map.getLayer('imagery'));
+  var imagery = new OpenLayers.Layer.Image(
+    'imagery',
+    app.info.url,
+    OpenLayers.Bounds.fromString(app.info.bbox), // No transform here
+    new OpenLayers.Size(app.info.width, app.info.height),
+    {
+      opacity: 1.0, 
+      isBaseLayer: false,
+      numZoomLevels: 20,
+      alwaysInRange: true
+    }
+  );
+  app.map.addLayer(imagery);
+  app.map.setLayerIndex(imagery, 1);
+}
+App.prototype.loadImagery = App.prototype.loadImagery;
 
-App.prototype.loadGuide = function() {
+App.prototype.getOneMeter = function (projection, lonlat) {
+  var wgs84 = new OpenLayers.Projection("EPSG:4326");
+
+  var p1 = lonlat.transform(projection, wgs84);
+  var p2 = p1.add(0, 1.0 / 1852 / 60);
+  p1 = p1.transform(wgs84, projection);
+  p2 = p2.transform(wgs84, projection);
+
+  return p2.lat - p1.lat;
+}
+App.prototype.guideStyle = {
+  strokeColor: "#000000",
+  strokeWidth: 3,
+  strokeOpacity: 0.5,
+  fillOpacity: 0,
+  fillColor: "#000000"
+};
+
+App.prototype.loadGuideCrossHair = function() {
   var app = this;
   var guide = app.map.getLayer('guide');
-  guide.removeAllFeatures();
-
-  var p1 = new OpenLayers.LonLat(app.info.longitude, app.info.latitude).transform(
-    app.map.getProjectionObject(),
-    new OpenLayers.Projection("EPSG:4326"));
-  var p2 = p1.add(0, 1.0 / 1852 / 60);
-  p1 = p1.transform(new OpenLayers.Projection("EPSG:4326"), app.map.getProjectionObject());
-  p2 = p2.transform(new OpenLayers.Projection("EPSG:4326"), app.map.getProjectionObject());
-  var oneMeter = p2.lat - p1.lat;
-
-  var center = new OpenLayers.Geometry.Point(app.info.longitude, app.info.latitude);
-
-  var style = {
-    strokeColor: "#000000",
-    strokeWidth: 3,
-    strokeOpacity: 0.5,
-    fillOpacity: 0,
-    fillColor: "#000000"
-  };
-
+  var oneMeter = app.getOneMeter(app.map.getProjectionObject(), 
+    new OpenLayers.LonLat(app.info.longitude, app.info.latitude).transform(
+      app.taskProjection,
+      app.map.getProjectionObject()));
   guide.addFeatures([
     new OpenLayers.Feature.Vector(
       new OpenLayers.Geometry.LineString([
         new OpenLayers.Geometry.Point(app.info.longitude - app.info.size * oneMeter, app.info.latitude),
         new OpenLayers.Geometry.Point(app.info.longitude + app.info.size * oneMeter, app.info.latitude)]),
-      null, style),
+      null, app.guideStyle),
     new OpenLayers.Feature.Vector(
       new OpenLayers.Geometry.LineString([
         new OpenLayers.Geometry.Point(app.info.longitude, app.info.latitude - app.info.size * oneMeter),
         new OpenLayers.Geometry.Point(app.info.longitude, app.info.latitude + app.info.size * oneMeter)]),
-      null, style)]);
+      null, app.guideStyle)]);
 
-  var circleGeom = OpenLayers.Geometry.Polygon.createRegularPolygon(center, app.info.size * oneMeter, 20, 0);
+}
+App.prototype.loadGuideCircle = function() {
+  var app = this;
+  var guide = app.map.getLayer('guide');
+  var center = new OpenLayers.Geometry.Point(app.info.longitude, app.info.latitude);
+  var oneMeter = app.getOneMeter(app.map.getProjectionObject(), 
+    new OpenLayers.LonLat(app.info.longitude, app.info.latitude).transform(
+      app.taskProjection,
+      app.map.getProjectionObject()));
+  var circleGeom = OpenLayers.Geometry.Polygon.createRegularPolygon(center, app.info.size * oneMeter, 40, 0);
   var circle = new OpenLayers.Feature.Vector(
     circleGeom,
     null,
-    style
+    app.guideStyle
   );
   guide.addFeatures([circle]);
-  app.taskBounds = circleGeom.getBounds().scale(1.2);
+}
+App.prototype.loadGuide = function() {
+  var app = this;
+  var guide = app.map.getLayer('guide');
+  guide.removeAllFeatures();
+
+  app.loadGuideCrossHair();
+  app.loadGuideCircle();
 }
 
 App.prototype.updateMap = function(info) {
--- cricketfrog/maphandler.js
+++ cricketfrog/maphandler.js
@@ -7,22 +7,16 @@ App.prototype.init = function () {
   var app = this;
   return app;
 }
+
 App.prototype.drawStyle = 'new';
-App.prototype.loadMapCreateMap = function() {
-  var app = this;
-  app.map = new OpenLayers.Map({
-      div: "map",
-      controls: [],
-  });
-}
+App.prototype.useMapUnderlayes = true;
+// When using OpenLayers.Layer.Image the task projection must be the
+// same as the map projection, which comes from Google Maps if using
+// map underlays...
+App.prototype.taskProjection = new OpenLayers.Projection("EPSG:900913");
+
 App.prototype.loadMapAddLayers = function() {
   var app = this;
-
-  app.layers.osm = new OpenLayers.Layer.OSM();
-  app.map.addLayer(app.layers.osm);
-
-  app.layers.gmap = new OpenLayers.Layer.Google("Satellite", {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22});
-  app.map.addLayer(app.layers.gmap);
 
   BaseOpenlayersApp.prototype.loadMapAddLayers.call(this);
 
@@ -35,8 +29,6 @@ App.prototype.loadMapAddLayers = function() {
 App.prototype.loadMapAddControls = function() {
   var app = this;
   BaseOpenlayersApp.prototype.loadMapAddControls.call(this);
-
-  app.map.addControl(new OpenLayers.Control.LayerSwitcher());
 
   if (app.drawStyle == 'new')
     app.layers.drillpads.mod = new OpenLayers.Control.DrawFeature(app.layers.drillpads, OpenLayers.Handler.Polygon);
@@ -80,55 +72,15 @@ App.prototype.getTaskBounds = function() {
   return OpenLayers.Bounds.fromString(app.info.bbox);
 }
 
-App.prototype.loadImagery = function() {
-  var app = this;
-  if (app.map.getLayer('imagery')) app.map.removeLayer(app.map.getLayer('imagery'));
-  var imagery = new OpenLayers.Layer.Image(
-    'imagery',
-    app.info.url,
-    OpenLayers.Bounds.fromString(app.info.bbox),
-    new OpenLayers.Size(app.info.width, app.info.height),
-    {
-      opacity: 1.0, 
-      isBaseLayer: false,
-      numZoomLevels: 20,
-      alwaysInRange: true
-    }
-  );
-  app.map.addLayer(imagery);
-  app.map.setLayerIndex(imagery, 1);
-}
+App.prototype.loadImagery = BaseOpenlayersApp.prototype.loadImageryIMG;
 
 App.prototype.loadGuide = function() {
   var app = this;
+  var guide = app.map.getLayer('guide');
+  guide.removeAllFeatures();
 
   if (app.drawStyle == 'new') {
-    var bbox = OpenLayers.Bounds.fromString(app.info.bbox);
-    var center = bbox.getCenterLonLat();
-
-    var guide = app.map.getLayer('guide');
-    guide.removeAllFeatures();
-
-    var p1 = new OpenLayers.LonLat(center.lon, center.lat).transform(
-      app.map.getProjectionObject(),
-      new OpenLayers.Projection("EPSG:4326"));
-    var p2 = p1.add(0, 1.0 / 1852 / 60);
-    p1 = p1.transform(new OpenLayers.Projection("EPSG:4326"), app.map.getProjectionObject());
-    p2 = p2.transform(new OpenLayers.Projection("EPSG:4326"), app.map.getProjectionObject());
-    var oneMeter = p2.lat - p1.lat;
-
-    guide.addFeatures([new OpenLayers.Feature.Vector(
-      OpenLayers.Geometry.Polygon.createRegularPolygon(new OpenLayers.Geometry.Point(center.lon, center.lat), 125 * oneMeter, 20, 0),
-      null,
-      {
-        strokeColor: "#000000",
-        strokeWidth: 3,
-        strokeOpacity: 0.5,
-        fillOpacity: 0,
-        fillColor: "#000000"
-      }
-    )]);
-
+    app.loadGuideCircle();
   }
 }
 
@@ -142,4 +94,9 @@ App.prototype.getAnswer = function() {
   }
   drillpads.mod.activate();
   return app.answer;
+}
+
+App.prototype.updateMap = function(info) {
+  if (!info.size) info.size = 125;
+  BaseOpenlayersApp.prototype.updateMap.call(this, info);
 }
