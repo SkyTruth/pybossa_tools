cdece552e5adf79b0e3122c8adb40f1eb996df7e
Egil Moeller <egil.moller@freecode.no>
2013-11-19 18:24:14 +0100

    Added tasks

--- cricketfrog-padmapper/convert.sh
+++ cricketfrog-padmapper/convert.sh
@@ -1,11 +1,18 @@
 find "$1" -name "*.txt" |
   while read file; do
-    tail -n +2 $file |
-      recode text/cr-lf..text |
-      tr "	" "," |
-      sed -e "s+ \([0-9]*\)$+,\1+g" |
-      while IFS="," read longitude latitude year county url siteID bbox width height rest; do
-        bbox="$(echo $bbox | sed -e "s+\([-.0-9]*\) \([-.0-9]*\) \([-.0-9]*\) \([-.0-9]*\)$+\3,\4,\1,\2+g")"
-        echo "{\"url\": \"$url\", \"bbox\": \"$bbox\", \"width\": $width, \"height\": $height, \"siteID\":\"$siteID\"}"
-      done > "$(echo "$file" | sed -e "s+.txt$+.json+g")";
+    {
+      echo -n "["
+      first=true
+      tail -n +2 $file |
+        recode text/cr-lf..text |
+        tr "	" "," |
+        sed -e "s+ \([0-9]*\)$+,\1+g" |
+        while IFS="," read longitude latitude year county url siteID bbox width height rest; do
+          if [ "$first" != "true" ]; then echo -n ","; fi
+          first=false
+          bbox="$(echo $bbox | sed -e "s+\([-.0-9]*\) \([-.0-9]*\) \([-.0-9]*\) \([-.0-9]*\)$+\3,\4,\1,\2+g")"
+          echo -n "{\"url\": \"$url\", \"bbox\": \"$bbox\", \"width\": $width, \"height\": $height, \"siteID\":\"$siteID\"}"
+        done
+      echo "]"
+    } > "$(echo "$file" | sed -e "s+.txt$+.json+g")";
   done
