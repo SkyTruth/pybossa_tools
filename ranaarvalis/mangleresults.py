2c5cdda00060113e3b1011d4bba9a090dc931342
Egil Moeller <egil.moller@freecode.no>
2014-01-02 18:00:58 +0100

    Changed mangle.py to work with the combined file format

--- ranaarvalis/mangleresults.py
+++ ranaarvalis/mangleresults.py
@@ -5,33 +5,8 @@ import json
 import geojson
 import shapely
 
-# Task layout
-# {"info": {
-#         "n_answers": 30,
-#         "url": "https://mapsengine.google.com/06136759344167181854-15360160187865123339-4/wms/",
-#         "question": "Please click on any fracking pond(s) you see in this map",
-#         "longitude": -77.082574,
-#         "county": "Tioga",
-#         "state": "PA",
-#         "SiteID": "c94b8c55-da69-5cc4-b40f-67396f49974b",
-#         "bbox": "-77.0897987266,41.8422220145,-77.0753492734,41.8530259804",
-#         "year": "2010",
-#         "latitude": 41.847624,
-#         "options": {
-#             "layers": "06136759344167181854-16779344390631852423-4",
-#             "version": "1.3.0"}},
-#  "n_answers": 30,
-#  "quorum": 0,
-#  "calibration": 0,
-#  "created": "2013-11-04T17:32:36.187495",
-#  "app_id": 15,
-#  "state": "completed",
-#  "id": 3294,
-#  "priority_0": 0.0}
-
 # Task run layout
-# {"info": {
-#         "positions": [{"lat": 41.935984746031, "lon": -77.025829871467}]},
+# {"info": {"positions": [{"lat": 41.935984746031, "lon": -77.025829871467}]},
 #  "user_id": 10,
 #  "task_id": 3217,
 #  "created": "2013-11-04T19:14:07.984752",
@@ -40,23 +15,42 @@ import shapely
 #  "app_id": 15,
 #  "user_ip": null,
 #  "timeout": null,
-#  "id": 4034}
+#  "id": 4034,
+#  "task": {"info": {"n_answers": 30,
+#                    "url": "https://mapsengine.google.com/06136759344167181854-15360160187865123339-4/wms/",
+#                    "question": "Please click on any fracking pond(s) you see in this map",
+#                    "longitude": -77.082574,
+#                    "county": "Tioga",
+#                    "state": "PA",
+#                    "SiteID": "c94b8c55-da69-5cc4-b40f-67396f49974b",
+#                    "bbox": "-77.0897987266,41.8422220145,-77.0753492734,41.8530259804",
+#                    "year": "2010",
+#                    "latitude": 41.847624,
+#                    "options": {
+#                        "layers": "06136759344167181854-16779344390631852423-4",
+#                        "version": "1.3.0"}},
+#           "n_answers": 30,
+#           "quorum": 0,
+#           "calibration": 0,
+#           "created": "2013-11-04T17:32:36.187495",
+#           "app_id": 15,
+#           "state": "completed",
+#           "id": 3294,
+#           "priority_0": 0.0}}
+
+
+if len(sys.argv) != 2:
+    print """
+Usage: mangleresults.py combined.json > points.geojson
+"""
+    sys.exit(0)
 
 with open(sys.argv[1]) as f:
-    tasks = json.loads(f.read())
-
-task_idx_by_id = {}
-for idx, task in enumerate(tasks):
-    task_idx_by_id[task['id']] = idx
-
-with open(sys.argv[2]) as f:
     taskruns = json.loads(f.read())
-
-for task in tasks:
-    task.update(task.pop('info'))
 
 for taskrun in taskruns:
     taskrun['taskrun_id'] = taskrun.pop('id')
+    taskrun['task']['task_id'] = taskrun['task'].pop('id')
 
 
 print '{"type": "FeatureCollection", "features": ['
@@ -64,12 +58,11 @@ print '{"type": "FeatureCollection", "features": ['
 nrofpoints = 0
 first = True
 for taskrun in taskruns:
-    task = tasks[task_idx_by_id[taskrun['task_id']]]
-
     info = {}
-    info.update(task)
+    info.update(taskrun['task']['info'])
     taskrundata = dict(taskrun)
     taskrundata.pop('info')
+    taskrundata.pop('task')
     info.update(taskrundata)
 
     if 'latitude' in info: info['old_latitude'] = info.pop('latitude')
