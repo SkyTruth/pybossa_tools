7ae5a0b6c618618ce757e0336c3384689b0794ce
Kevin Wurster <kevin@skytruth.org>
2014-03-27 13:15:50 -0400

    Initial commit

---
+++ experimental/getUnifinishedTasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Copyright SkyTruth 2014
+
+import json
+from os.path import isfile
+
+
+# Build information
+__author__ = 'Kevin Wurster'
+__version__ = '0.1'
+__doc__ = '''
+Utility for extracting incomplete tasks from PyBossa
+'''
+
+
