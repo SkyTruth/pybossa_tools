c93e1757a325f288f1a0e989dd206f9f1b042cdd
Kevin Wurster <kevin@skytruth.org>
2014-04-03 16:28:05 -0400

    Moved some stuff from main() to fuctions: get_unique_pond_ids(), get_crowd_selection(), get_task_attributes()

--- experimental/combined2shp.py
+++ experimental/combined2shp.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 
+from __future__ import division
+import ogr
+import osr
+import sys
+import json
+from os.path import isfile
+from os.path import basename
+
+
 __author__ ='Kevin Wurster'
 __organization__ = 'SkyTruth'
 __doc__ = """
@@ -10,19 +19,100 @@ metrics.
 """
 
 
-import ogr
-import osr
-import sys
-import json
-from os.path import isfile
-from os.path import basename
-
-
 def print_usage():
+    """
+    Command line usage information
+    """
     print("")
     print("Usage: combined.json outfile.shp")
     print("")
     return 1
+
+
+def get_unique_pond_ids(json_object, id_field='task_id'):
+    """
+    Get a list of unique pond ID's
+    """
+    return set([i[id_field] for i in json_object])
+
+
+def get_crowd_selection(selection_count):
+    """
+    Figure out what the crowd actually selected
+    """
+
+    # Cache containers
+    crowd_selection = 'NONE'
+
+    # Figure out what the maximum number of selections was
+    max_selection = max(selection_count.values())
+
+    # Build the crowd_selection
+    for selection, count in selection_count.iteritems():
+        if crowd_selection == 'NONE':
+            crowd_selection = selection
+        else:
+            crowd_selection += '|' + selection
+
+    # Return to user
+    return crowd_selection
+
+
+def get_task_attributes(input_id, json_object):
+    """
+    Collect attributes for a given task/pond ID
+    """
+
+    # Container for caching all attributes
+    attributes = {}
+
+    # Loop through all the input combined tasks in the json_object
+    for item in json_object:
+
+        # If the input id matches the foreign key in a combined task, pull some attributes
+        if str(item['task_id']) == str(input_id):
+
+            # Get attributes
+            attributes['wms_url'] = item['task']['info']['url']
+            attributes['latitude'] = item['task']['info']['latitude']
+            attributes['longitude'] = item['task']['info']['longitude']
+            attributes['year'] = item['task']['info']['year']
+            attributes['county'] = item['task']['info']['county']
+
+            # Tally categorizations
+            # Try/except is for a weird situation where item['info']['selection'] raises a KeyError exception
+            # No idea why this happens
+            attributes['n_unk_res'] = 0
+            attributes['n_frk_res'] = 0
+            attributes['n_oth_res'] = 0
+            try:
+                categorization = item['info']['selection']
+                if categorization == 'unknown':
+                    attributes['n_unk_res'] += 1
+                elif categorization == 'fracking':
+                    attributes['n_frk_res'] += 1
+                elif categorization == 'other':
+                    attributes['n_oth_res'] += 1
+            except KeyError:
+                attributes['n_unk_res'] = 0
+                attributes['n_frk_res'] = 0
+                attributes['n_oth_res'] = 0
+
+            # Compute the total number of responses
+            attributes['n_tot_res'] = attributes['n_unk_res'] + attributes['n_frk_res'] + attributes['n_oth_res']
+
+            # Handle some 'NoneType' weirdness - no idea why this happens
+            if attributes['wms_url'] is None:
+                attributes['wms_url'] = 'NONE'
+
+        # Figure out what the crowd selected
+        response_map = {'fracking': attributes['n_frk_res'],
+                        'other': attributes['n_oth_res'],
+                        'unknown': attributes['n_unk_res']}
+        attributes['crowd_sel'] = get_crowd_selection(response_map)
+
+    # Return final set of attributes
+    return attributes
 
 
 def main(args):
@@ -45,12 +135,8 @@ def main(args):
 
         # Get list of unique pond ID's
         print("Getting list of unique pond ID's...")
-        unique_pond_ids = []
-        for entry in loaded_json:
-            upi = entry['task_id']
-            if upi not in unique_pond_ids:
-                unique_pond_ids.append(upi)
-        total_unique_pond_ids = len(unique_pond_ids)
+        unique_pond_ids = get_unique_pond_ids(loaded_json)
+        total_unique_pond_ids = len(unique_pond_ids)  # This is used later to prevent calculating len for every task
         print("Found %s unique pond ID's" % str(total_unique_pond_ids))
 
         # == Create output shapefile == #
@@ -88,14 +174,17 @@ def main(args):
         field_name = ogr.FieldDefn('crowd_sel', ogr.OFTString)
         field_name.SetWidth(254)
         layer.CreateField(field_name)
+        field_name = ogr.FieldDefn('crowd_agr', ogr.OFTReal)
+        field_name.SetWidth(10)
+        layer.CreateField(field_name)
         field_name = ogr.FieldDefn('qaqc', ogr.OFTString)
         field_name.SetWidth(254)
         layer.CreateField(field_name)
         field_name = ogr.FieldDefn('county', ogr.OFTString)
         field_name.SetWidth(254)
         layer.CreateField(field_name)
-        field_name = ogr.FieldDefn('year', ogr.OFTString)
-        field_name.SetWidth(254)
+        field_name = ogr.FieldDefn('year', ogr.OFTInteger)
+        field_name.SetWidth(10)
         layer.CreateField(field_name)
         field_name = ogr.FieldDefn('wms_url', ogr.OFTString)
         field_name.SetWidth(254)
@@ -109,100 +198,33 @@ def main(args):
             i += 1
             print("Processing %s of %s" % (i, str(total_unique_pond_ids)))
 
-            # Reset field values
-            n_unk_res = 0
-            n_frk_res = 0
-            n_oth_res = 0
-            n_tot_res = 0
-            county = None
-            year = None
-            wms_url = None
-            latitude = None
-            longitude = None
-            categorization = None
-
-            # Get all responses associated with a given pond ID
-            for item in loaded_json:
-
-                if item['task_id'] == pond_id:
-
-                    # Get attributes
-                    wms_url = item['task']['info']['url']
-                    latitude = item['task']['info']['latitude']
-                    longitude = item['task']['info']['longitude']
-                    year = item['task']['info']['year']
-                    county = item['task']['info']['county']
-
-                    # Tally categorizations
-                    # Try/except is for a weird situation where item['info']['selection'] raises a KeyError exception
-                    # No idea why this happens
-                    try:
-                        categorization = item['info']['selection']
-                        if categorization == 'unknown':
-                            n_unk_res += 1
-                        elif categorization == 'fracking':
-                            n_frk_res += 1
-                        elif categorization == 'other':
-                            n_oth_res += 1
-                        else:
-                            print("ERROR: Invalid categorization")
-                            print("       pond id = %s" % pond_id)
-                            print("       class   = %s" % categorization)
-                    except KeyError:
-                        n_unk_res = 0
-                        n_frk_res = 0
-                        n_oth_res = 0
-
-            # Compute the total number of responses
-            n_tot_res = n_unk_res + n_frk_res + n_oth_res
-
-            # == Get Crowd Selection == #
-
-            # Create an easy way to reference list elements to what is contained in the list
-            response_map = {0: 'unknown',
-                            1: 'fracking',
-                            2: 'other'}
-
-            # Get the largest number of responses
-            crowd_selection = None
-            num_responses = [n_unk_res, n_frk_res, n_oth_res]
-            maximum_num_responses = max(num_responses)
-            for i in num_responses:
-                if i is maximum_num_responses:
-                    if crowd_selection is not None:
-                        crowd_selection = crowd_selection + '|' + response_map[i]
-                    else:
-                        crowd_selection = response_map[i]
-
-            # Handle some 'NoneType' weirdness - no idea why this happens
-            if wms_url is None:
-                wms_url = "None"
+            # Get the pond/site attributes
+            site_attributes = get_task_attributes(pond_id, loaded_json)
 
             # Update user
-            print("  wms_url   = %s" % wms_url[:40] + ' ...(truncated...)')
-            print("  pond_id   = %s" % pond_id)
-            print("  n_unk_res = %s" % str(n_unk_res))
-            print("  n_frk_res = %s" % str(n_frk_res))
-            print("  n_oth_res = %s" % str(n_oth_res))
-            print("  n_tot_res = %s" % str(n_tot_res))
-            print("  crowd_sel = %s" % crowd_selection)
-            print("  latitude  = %s" % str(latitude))
-            print("  longitude = %s" % str(longitude))
-            print("  county    = %s" % county)
-            print("  year      = %s" % year)
+            print("  wms_url   = %s" % site_attributes['wms_url'][:40] + ' ...(truncated...)')
+            print("  pond_id   = %s" % site_attributes['pond_id'])
+            print("  n_unk_res = %s" % str(site_attributes['n_unk_res']))
+            print("  n_frk_res = %s" % str(site_attributes['n_frk_res']))
+            print("  n_oth_res = %s" % str(site_attributes['n_oth_res']))
+            print("  n_tot_res = %s" % str(site_attributes['n_tot_res']))
+            print("  latitude  = %s" % str(site_attributes['latitude']))
+            print("  longitude = %s" % str(site_attributes['longitude']))
+            print("  county    = %s" % site_attributes['county'])
+            print("  year      = %s" % str(site_attributes['year']))
             print("  ")
 
             # Create the feature
             feature = ogr.Feature(layer.GetLayerDefn())
-            feature.SetField('pond_id', str(pond_id))
-            feature.SetField('n_unk_res', int(n_unk_res))
-            feature.SetField('n_frk_res', int(n_frk_res))
-            feature.SetField('n_oth_res', int(n_oth_res))
-            feature.SetField('n_tot_res', int(n_tot_res))
-            feature.SetField('county', str(county))
-            feature.SetField('year', str(year))
-            feature.SetField('wms_url', str(wms_url))
-            wkt = "POINT(%f %f)" % (float(longitude), float(latitude))
+            feature.SetField('pond_id', pond_id)
+            feature.SetField('n_unk_res', site_attributes['n_unk_res'])
+            feature.SetField('n_frk_res', site_attributes['n_frk_res'])
+            feature.SetField('n_oth_res', site_attributes['n_oth_res'])
+            feature.SetField('n_tot_res', site_attributes['n_tot_res'])
+            feature.SetField('county', site_attributes['county'])
+            feature.SetField('year', site_attributes['year'])
+            feature.SetField('wms_url', site_attributes['wms_url'])
+            wkt = "POINT(%f %f)" % (float(site_attributes['longitude']), float(site_attributes['latitude']))
             point = ogr.CreateGeometryFromWkt(wkt)
             feature.SetGeometry(point)
             layer.CreateFeature(feature)
