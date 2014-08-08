d3833912b668158a97e4b8d51f78e7df5ae151ca
Yolandita <Yolandita@skytruth.org>
2014-08-08 11:58:50 -0400

    decided to disable 'force users to click + and view in google maps' for now. commented it out

--- base-openlayers/tutorial.js
+++ base-openlayers/tutorial.js
@@ -48,7 +48,7 @@ Page.prototype.initializeStep = function() {
       page.initializedSteps[page.step].tasks = maptasks;
       page.app.loadMap();
       page.setMapTask();
-
+/*
       $('.expander-control').popover({html: true, content: 'Click <i class="icon-plus-sign"></i> to maximize the task box to see more information on this site.', title: 'The task box', trigger: "manual", placement: 'top'});
       $('.expander-control').popover('show');
       $(".btn").attr({disabled: "disabled"});
@@ -62,7 +62,7 @@ Page.prototype.initializeStep = function() {
 
         $('.latlonlink').popover({content: 'Click this link to open the site in Google Maps.', title: 'Google Maps', trigger: "manual", placement: 'left'});
         $('.latlonlink').popover('show');
-      });
+      }); */
     } else {
       page.initializedSteps[page.step] = true;
     }
