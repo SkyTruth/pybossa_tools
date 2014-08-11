ca093f81c3d405cab8a2fe297e36cf43fb01e1f8
Egil Moeller <egil.moller@freecode.no>
2014-08-11 11:53:43 +0200

    New pad digitizer supporting multiple imagery layers

--- digitizer-pad/maphandler.js
+++ digitizer-pad/maphandler.js
@@ -1,12 +1,14 @@
-CricketFrogPadMapperApp = App = function() {
+CricketFrogPondMapperApp = App = function() {
   CricketFrogApp.apply(this, arguments);
 };
 App.prototype = new CricketFrogApp();
 
 App.prototype.drawStyle = 'new';
-App.prototype.useMapUnderlayes = true;
-// When using OpenLayers.Layer.Image the task projection must be the
-// same as the map projection, which comes from Google Maps if using
-// map underlays...
-App.prototype.taskProjection = new OpenLayers.Projection("EPSG:900913");
-App.prototype.loadImagery = CricketFrogApp.prototype.loadImageryIMG;
+
+App.prototype.loadGuide = function() {
+  var app = this;
+  var guide = app.map.getLayer('guide');
+  guide.removeAllFeatures();
+
+  app.loadGuideCrossHair();
+}
